d345f9ce0231b4dd141cc63976f2efeaba97cad9
jay <nenojaafar3@gmail.com>
2018-05-15 14:31:25 -0500

    modified:   App.js passed pendingGuest to GuestList

--- src/App.js
+++ src/App.js
@@ -124,6 +124,7 @@ class App extends Component {
             setNameAt={this.setNameAt}
             isFiltered={this.state.isFiltered}
             removeGuestAt={this.removeGuestAt}
+            pendingGuest={this.state.pendingGuest}
           />
         </div>
 
